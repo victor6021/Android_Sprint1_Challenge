8fe632246b4a086a625455bea71ccef7f992898e
victor6021 <victor60216021@gmail.com>
2019-08-10 03:19:21 -0700

    layouts are completed,the codes are 3/4 of the way down.
    updated: still working on the seekbar.
    
    updated: adding the life cycle and see if something will pop up in my brain.
    
    updated: finally got it done, on watched movies the names have "55555" behind them.

--- app/src/main/java/com/example/sprint1_challenege/ui/EditActivity.kt
+++ app/src/main/java/com/example/sprint1_challenege/ui/EditActivity.kt
@@ -4,50 +4,61 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.toColorInt
 import com.example.sprint1_challenege.R
 import com.example.sprint1_challenege.model.Movie
 import kotlinx.android.synthetic.main.activity_edit.*
 
-class EditActivity : AppCompatActivity(){
+class EditActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+    companion object {
+        const val WATCH_KEY = "WATCHED_KEY"
+    }
 
-        seek_bar_watch.max = 1
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_edit)
+            var seek = seek_bar_watch
+            seek.max = 1
 
-        save_button.setOnClickListener {
-            var saveMovieIntent = Intent()
-            saveMovieIntent.putExtra("movie", createMovie())
-            setResult(Activity.RESULT_OK, saveMovieIntent)
-            finish()
+            save_button.setOnClickListener {
+                var rating = seek.progress
+                var saveMovieIntent = Intent()
+
+                saveMovieIntent.putExtra("MOVIE", watchedMovie())
+                saveMovieIntent.putExtra(WATCH_KEY, rating)
+                saveMovieIntent.putExtra("movie", createMovie())
+                setResult(Activity.RESULT_OK, saveMovieIntent)
+                finish()
+            }
+
+            delete_button.setOnClickListener {
+                var deleteMovieIntent = Intent()
+                deleteMovieIntent.putExtra("delete", deleteMovie())
+                setResult(Activity.RESULT_CANCELED, deleteMovieIntent)
+                finish()
+            }
+
+            var bundle: Bundle? = intent.extras
+            if (bundle != null) {
+                loadMovie(bundle.getSerializable("Movie") as Movie)
+            }
         }
 
-        delete_button.setOnClickListener {
-        var deleteMovieIntent = Intent()
-        deleteMovieIntent.putExtra("delete", deleteMovie())
-        setResult(Activity.RESULT_OK, deleteMovieIntent)
-        finish()
-    }
-
-        var bundle: Bundle? = intent.extras
-        if (bundle != null) {
-            loadMovie(bundle.getSerializable("Movie") as Movie)
+        fun loadMovie(movie: Movie) {
+            edit_movie.setText(movie.title)
         }
+
+        fun createMovie(): Movie {
+            var newMovie = Movie(edit_movie.text.toString())
+            return newMovie
+        }
+        fun watchedMovie(): Movie {
+            var watched = Movie(edit_movie.text.toString().plus(55555))
+            return watched
     }
-
-    fun loadMovie(movie: Movie){
-        edit_movie.setText(movie.title)
-    }
-
-    fun createMovie(): Movie {
-        var newMovie = Movie(edit_movie.text.toString())
-        return newMovie
-    }
-
-
-    fun deleteMovie(): Movie{
+    fun deleteMovie(): Movie {
         var delMovie = Movie(edit_movie.text.clear().toString())
         return delMovie
     }
-}
+    }
--- app/src/main/java/com/example/sprint1_challenege/ui/ListActivity.kt
+++ app/src/main/java/com/example/sprint1_challenege/ui/ListActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import com.example.sprint1_challenege.R
 import com.example.sprint1_challenege.model.Movie
@@ -16,7 +15,8 @@ class ListActivity : AppCompatActivity() {
     var movielist = mutableListOf<Movie>()
 
     companion object {
-        const val REQUEST_CODE_EDIT_MOVIE = 2
+        const val REQUEST_CODE_EDIT_MOVIE = 30
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +26,7 @@ class ListActivity : AppCompatActivity() {
         add_movie_button.setOnClickListener {
             var addMovieIntent = Intent(this, EditActivity::class.java)
             startActivityForResult(addMovieIntent, REQUEST_CODE_EDIT_MOVIE)
+
         }
     }
 
@@ -46,64 +47,26 @@ class ListActivity : AppCompatActivity() {
         newMovieView.textSize = 20f
         newMovieView.id = index
         newMovieView.text = movie.title
-        var newMovieView1 = TextView(this)
-        newMovieView1.textSize = 40f
-        newMovieView1.id = index
-        newMovieView1.text = movie.title.capitalize()
 
         newMovieView.setOnClickListener {
             var movieIntent = Intent(this, EditActivity::class.java)
             movieIntent.putExtra("Movie", movielist[newMovieView.id])
             movielist.removeAt(newMovieView.id)
             startActivityForResult(movieIntent, REQUEST_CODE_EDIT_MOVIE)
-            if (seek_bar_watch.progress == 0) {
-                newMovieView
-            }
-            if (seek_bar_watch.progress == 1) {
-                movieIntent.putExtra("Movie", movielist[newMovieView1.id])
-                movielist.removeAt(newMovieView1.id)
-                startActivityForResult(movieIntent, REQUEST_CODE_EDIT_MOVIE)
-                newMovieView1
-            }
         }
         return newMovieView
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE_EDIT_MOVIE && resultCode == Activity.RESULT_OK) {
+        if (requestCode == REQUEST_CODE_EDIT_MOVIE && resultCode == Activity.RESULT_OK){
+            var rating = data?.getIntExtra(EditActivity.WATCH_KEY, 0)
             var newMovieResult = data!!.getSerializableExtra("movie") as Movie
-            movielist.add(newMovieResult)
-        }
+            var watchedResult = data.getSerializableExtra("MOVIE") as Movie
+            if (rating == 0)
+                movielist.add(newMovieResult)
+            if (rating != 0)
+                movielist.add(watchedResult)
     }
+}}
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("List_onStart", movielist.toString())
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("List_onResume", movielist.toString())
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("List_onPause", movielist.toString())
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("List_onStop", movielist.toString())
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("List_onDestroy", movielist.toString())
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("List_onRestart", movielist.toString())
-    }
-}
